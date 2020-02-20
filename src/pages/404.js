a2b6769351315b72d7e956d1302ae40cf5f88205
Fxva Skin <fxva@outlook.com>
2020-02-20 09:42:30 +0600

    add redirect to indexPage from 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,8 @@
-import React, { Fragment } from 'react'
-import SEO from 'components/seo'
-
-const NotFoundPage = () => (
-  <Fragment>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Fragment>
-)
+const NotFoundPage = () => {
+  if (typeof window !== 'undefined') {
+    window.location.replace('/')
+  }
+  return null
+}
 
 export default NotFoundPage
