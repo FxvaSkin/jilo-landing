4753dd7f5cc6bff168f1230f33b153630afc6683
Fxva Skin <fxva@outlook.com>
2020-02-25 01:48:05 +0600

    flex layout fixed

--- src/containers/Map/index.jsx
+++ src/containers/Map/index.jsx
@@ -11,6 +11,59 @@ import accessToken from 'token'
 import { branches } from './branches'
 import { MarkerIcon } from './MarkerIcon'
 
+const PhoneIcon = props => (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    fill="none"
+    shape-rendering="geometricPrecision"
+    {...props}
+  >
+    <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z" />
+  </svg>
+)
+
+const AddressIcon = props => (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    fill="none"
+    shape-rendering="geometricPrecision"
+    {...props}
+  >
+    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0118 0z" />
+    <circle cx="12" cy="10" r="3" />
+  </svg>
+)
+
+const ClockIcon = props => (
+  <svg
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    fill="none"
+    shape-rendering="geometricPrecision"
+    {...props}
+  >
+    <circle cx="12" cy="12" r="10" />
+    <path d="M12 6v6l4 2" />
+  </svg>
+)
+
 let ReactMapboxGl = {}
 let Layer, ZoomControl, RotationControl, Popup, Marker, Cluster
 
@@ -86,10 +139,24 @@ const Map = () => {
           coordinates={[selected.lng, selected.lat]}
         >
           <>
-            <b>{selected.tel}</b>
-            <br />
-            <i>{selected.hours}</i>
-            <p>{selected.address}</p>
+            <ul>
+              {selected.tels &&
+                selected.tels.map(tel => (
+                  <li key={tel} className={styles.phone}>
+                    <a href={`tel:${tel}`}>
+                      <PhoneIcon className={styles.icon} /> {tel}
+                    </a>
+                  </li>
+                ))}
+            </ul>
+            <p className={styles.address}>
+              <AddressIcon className={styles.icon} />
+              {selected.address}
+            </p>
+            <p className={styles.clock}>
+              <ClockIcon className={styles.icon} />
+              {selected.hours}
+            </p>
 
             <button onClick={handleClose} className={styles.popupCloseButton}>
               <svg
